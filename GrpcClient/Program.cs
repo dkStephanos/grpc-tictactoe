b5f073d120a05d2850f194e5b461bc9cf54360fd
dkStephanos <stephanos@goldmail.etsu.edu>
2020-10-15 12:25:11 -0400

    Added comments

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -9,17 +9,23 @@ namespace GrpcClient
    {
       static async Task Main(string[] args)
       {
-         using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            // Establish the gRPC channel and intialize a new instance of the TicTacToe client on that channel
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new TicTacToe.TicTacToeClient(channel);
+            
+            // Initiate the greeting
             var greetResponse = await client.GreetPlayerAsync(
                 new GreetRequest { GreetRequestMsg = "Hello, I would like to play!" });
 
+            // Print greeting for user, collecting player token from user
             Console.WriteLine(greetResponse.GreetResponseMsg);
             string playerToken = Console.ReadLine(); 
 
+            // Initialize a new game, passing along the token collected from the user
             CurrentGameBoard reply = await client.NewGameAsync(
                 new PlayerToken { PlayerTokenMsg = playerToken });
 
+            // Set up a loop to confirm we get a valid token, repeating the new game request until a valid choice is provided
             while(reply.GameMsg.Contains("Invalid"))
             {
                 Console.WriteLine(reply.GameMsg);
@@ -28,22 +34,24 @@ namespace GrpcClient
                     new PlayerToken { PlayerTokenMsg = playerToken });
             }
 
+            // Then enter the main game loop, continuing until a final outcome is reached
             string nextMove;
             while(!reply.GameMsg.Contains("won!") && !reply.GameMsg.Contains("Draw"))
             {
                 Console.WriteLine($"{reply.Board}\n{reply.GameMsg}\n");
 
+                // Collect the move from the user, passing along to the next turn method (error checking provided on server)
                 nextMove = Console.ReadLine();
                 reply = await client.PlayNextTurnAsync(
                     new PlayerMove { BoardLocation = nextMove });
             }
          
-         // Print the final game board with message
-         Console.WriteLine($"{reply.Board}\n{reply.GameMsg}\n");
+           // Print the final game board with message
+           Console.WriteLine($"{reply.Board}\n{reply.GameMsg}\n");
          
-            
-         Console.WriteLine("Press any key to exit...");
-         Console.ReadKey();
+           // Wait for input to exit client
+           Console.WriteLine("Press any key to exit...");
+           Console.ReadKey();
       }
    }
 }
--- GrpcServer/Services/TicTacToeService.cs
+++ GrpcServer/Services/TicTacToeService.cs
@@ -20,7 +20,7 @@ namespace GrpcServer
 
         public override Task<GreetResponse> GreetPlayer(GreetRequest request, ServerCallContext context)
         {
-
+            // Sends the greet message to the client, prompting for token selection
             return Task.FromResult(new GreetResponse
             {
                 GreetResponseMsg = _currGame.GetGreetingPrompt()
@@ -29,9 +29,11 @@ namespace GrpcServer
 
         public override Task<CurrentGameBoard> NewGame(PlayerToken request, ServerCallContext context)
         {
+            // Initialize standard new game prompt
             string gameMsg = "\nWhere would you like to go?";
             char playerToken;
 
+            // Check to make sure the token provided is of type char and matches either X or O, otherwise send an error msg and reprompt for valid input
             if(Char.TryParse(request.PlayerTokenMsg, out playerToken) && (playerToken == 'X' || playerToken == 'O'))
             {
                 _currGame.selectPlayerToken(char.Parse(request.PlayerTokenMsg));
@@ -39,7 +41,8 @@ namespace GrpcServer
             {
                 gameMsg = "Invalid Selection: Must be X or O. Try again: ";
             }
-               
+            
+            // Sends the starting game board and prompt for move or error msg depending on valid token selection
             return Task.FromResult(new CurrentGameBoard
             {
                 Board = _currGame.board.drawBoard(),
@@ -49,10 +52,15 @@ namespace GrpcServer
 
         public override Task<CurrentGameBoard> PlayNextTurn(PlayerMove request, ServerCallContext context)
         {
+            // Set up game variables and initialize the error message if user entered invalid board location
             string gameMsg;
             string errorMsg = "Invalid Selection: Must be a number between 0-9. Try again: ";
             int boardLocation;
+
+            // Attempt to parse the boardLocation sent by the user
             bool validBoardLocation = Int32.TryParse(request.BoardLocation, out boardLocation);
+
+            // Confirm the boardLocation is an integer in the proper range, if so, attempt to play the turn, if not set the error message
             if (validBoardLocation && boardLocation <= 9 && boardLocation >= 0)
             {
                 gameMsg = _currGame.playTurn(boardLocation);
@@ -60,9 +68,11 @@ namespace GrpcServer
             {
                 gameMsg = errorMsg;
             }
-                
+            
+            // Get the current state of the game board to send to user, storing it in case we need to reset the board in case of end game
             string gameBoard = _currGame.board.drawBoard();
 
+            // If the game is over, clear the board for the next session, otherwise if the game is still going and we have no errors, set the gameMsg to prompt for next move
             if (gameMsg.Contains("won!") || gameMsg.Contains("Draw"))
             {
                 _currGame.resetBoard();
@@ -71,6 +81,7 @@ namespace GrpcServer
                 gameMsg = "Where would you like to go next?";
             }
 
+            // Finally, send the message with the current game board and gameMsg (either containing final result, prompt or error)
             return Task.FromResult(new CurrentGameBoard
             {
                 Board = gameBoard,
